773efec70a743b47e6749c0163d66ed9787a2e55
Mark Aigbe <b00118087@mytudublin.ie>
2021-03-15 21:52:28 +0000

    first commit of the project!

---
+++ src/project/Client.java
@@ -0,0 +1,9 @@
+package project;
+
+public class Client {
+    public static void main(String[] args){
+
+        System.out.println();
+        System.out.println("Welcome to my java game application");
+    }
+}
