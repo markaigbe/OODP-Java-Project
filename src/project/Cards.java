d03605887afc4cd9e8558e7efffc9cf47407533c
markaigbe <markaigbe@gmail.com>
2021-03-20 11:57:35 +0000

    first configs from laptop

--- src/project/Cards.java
+++ src/project/Cards.java
@@ -7,6 +7,67 @@ public class Cards {
     private int clubs;
     private int hearts;
     private int spades;
+    private int deck = 52; // 52
+    private int cardNum[]; // 1 -> 13
+    private String cardType;
     // need a variable deck (52), rank (13), type (bool)
 
+
+    public int getDiamonds() {
+        return diamonds;
+    }
+
+    public void setDiamonds(int diamonds) {
+        this.diamonds = diamonds;
+    }
+
+    public int getClubs() {
+        return clubs;
+    }
+
+    public void setClubs(int clubs) {
+        this.clubs = clubs;
+    }
+
+    public int getHearts() {
+        return hearts;
+    }
+
+    public void setHearts(int hearts) {
+        this.hearts = hearts;
+    }
+
+    public int getSpades() {
+        return spades;
+    }
+
+    public void setSpades(int spades) {
+        this.spades = spades;
+    }
+
+    public bool configureCards(){
+         for (int i=0; i<cardNum.length; i++){
+             cardNum[i] += cardNum[];
+             System.out.println();
+         }
+    }
+
+    public boolean checkCardType(){
+        if(cardType = "diamonds"){
+            // do something
+            return "diamonds" + true;
+        }
+        else if(cardType = "clubs"){
+            return "clubs" + true;
+        }
+        else if(cardType = "hearts"){
+            return "hearts" + true;
+        }
+        else if(cardType = "spades"){
+            return "spades" + true;
+        }
+        else {
+            return false;
+        }
+    }
 }
